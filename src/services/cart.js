e92fc0a40d707d436a8eb48216a9f6715d680ee2
vitoria <vitoriarntrindade@gmail.com>
2024-07-23 17:45:18 -0400

    assinatura dos metódos de carrinho

---
+++ src/services/cart.js
@@ -0,0 +1,16 @@
+async function addItem(userCart){
+
+}
+
+async function deleteItem(userCart, name){
+
+}
+
+async function removeItem(userCart, id){
+
+}
+
+async function calculateTotal(userCart){
+
+}
+
